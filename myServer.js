025c2180cfb3c53fcb1015490c557726ccfc1865
Finnedorn <daniele.giuntoli@outlook.com>
2024-05-21 14:45:03 +0200

    added new server settings to myServer.js / added new .env var / updated json file with new script

---
+++ myServer.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const http = require("http");
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+    console.log(req.method, req.url);
+    res.writeHead(200, {"Content-Type": "text/html" });
+    res.end(`
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>myServer</title>
+    </head>
+    <body>
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    </body>
+    </html>
+    `);
+});
+
+server.listen(port, host , () => {
+    console.log(`Ho avviato il server su http://${host}:${port}/`);
+});
